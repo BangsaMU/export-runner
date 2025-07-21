08f86c1d63f39fdd596e7494f771c0bda472ba0f
GITA <gita.samudra@meindo.com>
2025-07-21 14:38:33 +0700

    fix email send

--- src/Jobs/RunExportReportJob.php
+++ src/Jobs/RunExportReportJob.php
@@ -5,6 +5,7 @@ namespace Bangsamu\ExportRunner\Jobs;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -14,6 +15,7 @@ use Illuminate\Foundation\Bus\Dispatchable; // <== ini penting
 
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
 
 class RunExportReportJob implements ShouldQueue
 {
@@ -28,6 +30,8 @@ class RunExportReportJob implements ShouldQueue
         $this->reportId = $reportId;
         $this->params = $params;
         $this->email = $email;
+        $this->params['job_id'] = Str::uuid()->toString(); // 🆕 Generate UUID
+        $this->params['user'] = $this->params['user']??$email; // jika tidak ada user ambil dar iemail
     }
 
     public function handle(): void
@@ -65,19 +69,42 @@ class RunExportReportJob implements ShouldQueue
         // $process = new Process($args);
         try {
             $process->mustRun();
-            Log::info("Export success", ['output' => $process->getOutput()]);
 
-            $date = now()->format('ymd');
-            $filename = "report_{$this->reportId}.xlsx";
-            $filePath = base_path("go_reports/output/{$date}/{$filename}");
+            $jobId = $this->params['job_id'];
 
-            if (file_exists($filePath)) {
-                Mail::raw('Report is ready.', function ($message) use ($filePath) {
+            Log::info("Export success jobId::".$jobId , ['output' => $process->getOutput()]);
+
+            // $date = now()->format('ymd');
+            // $filename = "report_{$this->reportId}.xlsx";
+            // $filePath = base_path("go_reports/output/{$date}/{$filename}");
+
+            // if (file_exists($filePath)) {
+            //     Mail::raw('Report is ready.', function ($message) use ($filePath) {
+            //         $message->to($this->email)
+            //                 ->subject('Your Report')
+            //                 ->attach($filePath);
+            //     });
+            // }
+
+            $log = DB::table('report_log')
+                ->where('job_id', $jobId)
+                ->where('status', 'success')
+                ->orderByDesc('created_at')
+                ->first();
+
+
+            $path_file = storage_path($log->file_name);
+
+            if ($log && file_exists($path_file)) {
+                Mail::raw('Report is ready.', function ($message) use ($log,$path_file) {
                     $message->to($this->email)
                             ->subject('Your Report')
-                            ->attach($filePath);
+                            ->attach($path_file);
                 });
+            } else {
+                Log::warning("Report file:: ".$path_file." (".file_exists($path_file).") not found for job_id: $jobId");
             }
+
         } catch (ProcessFailedException $e) {
             Log::error("Export failed", ['error' => $e->getMessage()]);
         }
